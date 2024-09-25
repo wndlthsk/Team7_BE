ce55c6f7793a931aeda0394bcdd00eb21714c7f1
suhyeon7497 <nucc1186@gmail.com>
2024-09-25 23:22:05 +0900

    [feat] token 발급 및 검증 기능
    token을 발급하고 검증하는 기능을 하는 JWTUtil을 작성하였습니다.
    
    관련 이슈: #2

---
+++ src/main/java/team7/inplace/security/util/JWTUtil.java
@@ -0,0 +1,43 @@
+package team7.inplace.security.util;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.Jwts.SIG;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JWTUtil {
+
+    private final SecretKey secretKey;
+    private static final Long expiredTime = 300L;
+
+    public JWTUtil(@Value("${spring.jwt.secret}") String secretKey) {
+        this.secretKey = new SecretKeySpec(secretKey.getBytes(StandardCharsets.UTF_8),
+            SIG.HS256.key().build().getAlgorithm());
+    }
+
+    public String createToken(String username) {
+        return Jwts.builder()
+            .claim("username", username)
+            .issuedAt(new Date(System.currentTimeMillis()))
+            .expiration(new Date(System.currentTimeMillis() + expiredTime))
+            .signWith(secretKey)
+            .compact();
+    }
+
+    public String getUsername(String token) {
+        return Jwts.parser().verifyWith(secretKey).build()
+            .parseSignedClaims(token).getPayload()
+            .get("username", String.class);
+    }
+
+    public Boolean isExpired(String token) {
+        return Jwts.parser().verifyWith(secretKey).build()
+            .parseSignedClaims(token).getPayload()
+            .getExpiration().before(new Date());
+    }
+}
